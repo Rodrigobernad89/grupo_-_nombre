c9725009284a2bd4412f5bbd442840a0213528b8
Rodrigo Bernad <rodrigobernad89@gmail.com>
2020-09-15 20:28:22 -0300

    Hize cambios en css y agregue el archivo gracias.htmly el metodo post en app.js

--- site/app.js
+++ site/app.js
@@ -5,20 +5,23 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.get('/productos', (req,res) =>{
-    res.sendFile(__dirname + '/productos.html');
+    res.sendFile(__dirname + '/public/productos.html');
 })
 
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/carrito.html')
+    res.sendFile(__dirname + '/public/carrito.html')
 })
 
 app.get('/registrarse', (req,res)=>{
-    res.sendFile(__dirname + 'public/registrarse.html')
+    res.sendFile(__dirname + '/public/registrarse.html')
 })
 
+app.post('/registrarse', (req,res)=> {
+    res.sendfile(__dirname + '/public/gracias.html');
+})
 
 app.listen(3000, ()=> console.log('server corriendo'));

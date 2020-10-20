b2ab25779687bdb5997752ebf4d7bce9e986c0e5
saida <ssubelza23@gmail.com>
2020-10-20 17:13:51 -0300

    cree vista registrarse y restaurar contraseña. Modifique la barra de busqueda en el heaader

--- site-express/MJR/src/controllers/usersController.js
+++ site-express/MJR/src/controllers/usersController.js
@@ -2,6 +2,12 @@ const controller = {
     login:(req, res) => {
       res.render('users/login')
       },
+      register:(req, res) => {
+        res.render('users/registrarse')
+        },
+        restore:(req, res) => {
+          res.render('users/restaurarContraseña')
+          },
       
 }
 
--- site-express/MJR/src/routes/users.js
+++ site-express/MJR/src/routes/users.js
@@ -5,5 +5,7 @@ const usersController = require('../controllers/usersController');
 /* GET users listing. */
  
 router.get('/login', usersController.login);
+router.get('/register', usersController.register);
+router.get('/restore', usersController.restore);
 
 module.exports = router;

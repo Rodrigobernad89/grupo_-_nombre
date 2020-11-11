a81f2679eb0e8496e8e4d65cbd7b88dd1c898738
Rodrigo Bernad <rodrigobernad89@gmail.com>
2020-11-11 01:39:26 -0300

    Creo metodo Read,Add y Delete

--- Site/MJR/src/controllers/productsController.js
+++ Site/MJR/src/controllers/productsController.js
@@ -1,9 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const { validationResult } = require('express-validator');
+const productsFilePath = path.join(__dirname, '../database/db_products.json');
+const leerJson = () => JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
+
+
 const controller = {
     index: (req, res) => {
-        res.render('products/productos');
+        const productos = leerJson();
+		res.render('products/productos', {productos});
       },
     detail: (req, res) => {
-        res.render('products/detalle-producto');
+        const productos = leerJson();
+		let id = req.params.id;
+		const productoEncontrado = productos.find(producto => producto.id == id);
+		res.render('products/detalle-producto', {productoEncontrado,toThousand})
     },
     cart:(req, res) => {
         res.render('products/carrito');
@@ -11,9 +26,57 @@ const controller = {
     create:(req,res) => {
         res.render('products/formulario-creacion');
     },
+    store: (req, res) => {
+
+		const errors = validationResult(req);
+
+		if(errors.isEmpty()){
+			const productos = leerJson();
+			let nuevo_producto = {
+				id:productos[productos.length -1].id +1,
+				...req.body,
+				imagen:req.file.filename
+            }
+            
+			let nuevaDB = JSON.stringify([...productos,nuevo_producto],null,2);
+			fs.writeFileSync(productsFilePath,nuevaDB);
+			res.redirect('/products/');	
+		} else {
+			res.render("products/formulario-creacion", { errors: errors.errors});
+		}
+	},
     edit:(req,res) => {
-        res.render('products/formulario-editar');
+        const productos = leerJson();
+		let id = req.params.id;
+		productoAEditar = productos.find(producto => producto.id == id)
+		res.render('products/formulario-editar', {productoAEditar, id});
     },
+    update: (req, res) => {
+		var productos = leerJson();
+        let id = req.params.id;
+		productos = productos.map(producto => {
+			if(producto.id == id){
+				producto.nombre_producto = req.body.nombre_producto;
+                producto.descripcion = req.body.descripcion;
+                producto.indicaciones = req.body.indicaciones;
+				producto.precio = req.body.precio;
+				producto.imagen = req.file.filename;
+			}	
+			return producto;
+        });
+		console.log(id);
+		let baseEditada =JSON.stringify(productos,null,2);
+		fs.writeFileSync(productsFilePath,baseEditada);
+		res.redirect('/products');	
+	},
+    destroy:(req,res) => {
+            const productos = leerJson();
+            let id = req.params.id;
+            let productos_restantes = productos.filter(producto => producto.id != id);
+            let nueva_base = JSON.stringify(productos_restantes,null,2);
+            fs.writeFileSync(productsFilePath,nueva_base);
+            res.redirect('/products/');
+    }
 }
 
 module.exports = controller;
--- Site/MJR/src/routes/products.js
+++ Site/MJR/src/routes/products.js
@@ -1,11 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+const multer = require('multer');
+const validator = require("../Middlewares/rutas/validator");
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,path.join(__dirname, '../../public/images'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+
+  var upload = multer({ 
+        
+    storage: storage,
+
+    fileFilter: (req,file,cb) => {
+        const acceptedExtensions = ['.jpg', '.jpeg', '.png'];
+        const ext = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(ext)){
+            req.file = file;
+        }
+        cb(null, acceptedExtensions.includes(ext));
+    }
+})
+
 const productController = require('../controllers/productsController');
 
+
+
 router.get('/', productController.index); 
-router.get('/detalle', productController.detail);
-router.get('/carrito', productController.cart);
 router.get('/crear', productController.create);
-router.get('/editar', productController.edit);
+router.get('/carrito', productController.cart);
+router.get('/:id', productController.detail);
+router.post('/',upload.single('imagen'), validator.product,productController.store);
+router.get('/:id/edit', productController.edit);
+router.put('/:id',upload.single('imagen'),validator.product,productController.update)
+router.delete('/:id',productController.destroy)
 module.exports = router;
 

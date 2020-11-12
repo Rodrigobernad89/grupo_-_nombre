74f490f1c753aa7636ae6c67ad1cd8d759b4487d
saida <ssubelza23@gmail.com>
2020-11-11 22:48:27 -0300

    agregue la

--- Site/MJR/src/controllers/mainController.js
+++ Site/MJR/src/controllers/mainController.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../database/db_products.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const controller = {
     index: (req, res) => {
         res.render('index');
       },
+      search: (req, res) => {
+        const search =req.body.busqueda;
+        var ExpReg = new RegExp(search,'i');
+       
+       const products=productos.filter(p=>{
+         console.log(p.nombre_producto)
+         return  ExpReg.test(p.nombre_producto) == true
+       })
+      
+      res.render('products/search',{products})
+      }
+      
 }
 
 module.exports = controller;
--- Site/MJR/src/routes/main.js
+++ Site/MJR/src/routes/main.js
@@ -4,5 +4,5 @@ const mainController = require('../controllers/mainController.js')
 
 /* GET home page. */
 router.get('/', mainController.index)
-
+router.post('/search', mainController.search); 
 module.exports = router;

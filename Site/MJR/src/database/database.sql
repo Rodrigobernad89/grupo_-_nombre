0486402a083685eacd85d592363b10956c515fd7
mariac-la <celitalaje@gmail.com>
2020-12-03 16:06:22 -0300

    cambie el nombre de base de datos a 'proyecto_integrador_db' y agregue descripcion e indicaciones a database.sql

--- Site/MJR/src/database/database.sql
+++ Site/MJR/src/database/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `proyecto_integrador`.`products` (
+CREATE TABLE `proyecto_integrador_db`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `proyecto_integrador`.`products` (
   PRIMARY KEY (`id`));
 
 
-CREATE TABLE `proyecto_integrador`.`users` (
+CREATE TABLE `proyecto_integrador_db`.`users` (
 `id` INT NOT NULL AUTO_INCREMENT,
 `creation` TIMESTAMP NULL,
 `modification` TIMESTAMP NULL,
@@ -21,7 +21,7 @@ CREATE TABLE `proyecto_integrador`.`users` (
 `password` VARCHAR(100) NOT NULL,
 PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`extras` (
+CREATE TABLE `proyecto_integrador_db`.`extras` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -30,41 +30,41 @@ CREATE TABLE `proyecto_integrador`.`extras` (
   `active` TINYINT NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`roles` (
+CREATE TABLE `proyecto_integrador_db`.`roles` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`profiles` (
+CREATE TABLE `proyecto_integrador_db`.`profiles` (
 `id` INT NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(100) NOT NULL,
 `description` TEXT NOT NULL,
 PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`payment_methods` (
+CREATE TABLE `proyecto_integrador_db`.`payment_methods` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`payment_status` (
+CREATE TABLE `proyecto_integrador_db`.`payment_status` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `proyecto_integrador`.`shipping_status` (
+CREATE TABLE `proyecto_integrador_db`.`shipping_status` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`));
-CREATE TABLE `proyecto_integrador`.`order_status` (
+CREATE TABLE `proyecto_integrador_db`.`order_status` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`));
-CREATE TABLE `proyecto_integrador`.`cart` (
+CREATE TABLE `proyecto_integrador_db`.`cart` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -73,10 +73,10 @@ CREATE TABLE `proyecto_integrador`.`cart` (
   INDEX `fk_cart_users_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_cart_users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `proyecto_integrador`.`users` (`id`)
+    REFERENCES `proyecto_integrador_db`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`shipping` (
+CREATE TABLE `proyecto_integrador_db`.`shipping` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -86,10 +86,10 @@ CREATE TABLE `proyecto_integrador`.`shipping` (
   INDEX `fk_shipping_shipping_status_idx` (`shipping_status_id` ASC),
   CONSTRAINT `fk_shipping_shipping_status`
     FOREIGN KEY (`shipping_status_id`)
-    REFERENCES `proyecto_integrador`.`shipping_status` (`id`)
+    REFERENCES `proyecto_integrador_db`.`shipping_status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`payments` (
+CREATE TABLE `proyecto_integrador_db`.`payments` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -103,20 +103,20 @@ CREATE TABLE `proyecto_integrador`.`payments` (
   INDEX `fk_payments_payment_status_idx` (`payment_status_id` ASC) ,
   CONSTRAINT `fk_payments-users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `proyecto_integrador`.`users` (`id`)
+    REFERENCES `proyecto_integrador_db`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_payments_payment_methods`
     FOREIGN KEY (`payment_method_id`)
-    REFERENCES `proyecto_integrador`.`payment_methods` (`id`)
+    REFERENCES `proyecto_integrador_db`.`payment_methods` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_payments_payment_status`
     FOREIGN KEY (`payment_status_id`)
-    REFERENCES `proyecto_integrador`.`payment_status` (`id`)
+    REFERENCES `proyecto_integrador_db`.`payment_status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`roles_profiles` (
+CREATE TABLE `proyecto_integrador_db`.`roles_profiles` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `role_id` INT NOT NULL,
     `profile_id` INT NOT NULL,
@@ -125,15 +125,15 @@ CREATE TABLE `proyecto_integrador`.`roles_profiles` (
     INDEX `fk_profiles_roles_idx` (`profile_id` ASC) ,
     CONSTRAINT `fk_roles`
     FOREIGN KEY (`role_id`)
-    REFERENCES `proyecto_integrador`.`roles` (`id`)
+    REFERENCES `proyecto_integrador_db`.`roles` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
     CONSTRAINT `fk_profiles`
     FOREIGN KEY (`profile_id`)
-    REFERENCES `proyecto_integrador`.`profiles` (`id`)
+    REFERENCES `proyecto_integrador_db`.`profiles` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`users_roles` (
+CREATE TABLE `proyecto_integrador_db`.`users_roles` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `role_id` INT NOT NULL,
@@ -142,15 +142,15 @@ CREATE TABLE `proyecto_integrador`.`users_roles` (
   INDEX `fk_roles_idx` (`role_id` ASC) ,
   CONSTRAINT `fk_users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `proyecto_integrador`.`users` (`id`)
+    REFERENCES `proyecto_integrador_db`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_uses_roles`
     FOREIGN KEY (`role_id`)
-    REFERENCES `proyecto_integrador`.`roles` (`id`)
+    REFERENCES `proyecto_integrador_db`.`roles` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`items` (
+CREATE TABLE `proyecto_integrador_db`.`items` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `cart_id` INT NOT NULL,
   `product_id` INT NOT NULL,
@@ -161,20 +161,20 @@ CREATE TABLE `proyecto_integrador`.`items` (
   INDEX `fk_items_extras_idx` (`extras_id` ASC) ,
   CONSTRAINT `fk_items_cart`
     FOREIGN KEY (`cart_id`)
-    REFERENCES `proyecto_integrador`.`cart` (`id`)
+    REFERENCES `proyecto_integrador_db`.`cart` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_items_products`
     FOREIGN KEY (`product_id`)
-    REFERENCES `proyecto_integrador`.`products` (`id`)
+    REFERENCES `proyecto_integrador_db`.`products` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_items_extras`
     FOREIGN KEY (`extras_id`)
-    REFERENCES `proyecto_integrador`.`extras` (`id`)
+    REFERENCES `proyecto_integrador_db`.`extras` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-CREATE TABLE `proyecto_integrador`.`orders` (
+CREATE TABLE `proyecto_integrador_db`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `creation` TIMESTAMP NULL,
   `modification` TIMESTAMP NULL,
@@ -192,27 +192,27 @@ CREATE TABLE `proyecto_integrador`.`orders` (
   INDEX `fk_orders_order_status_idx` (`order_status_id` ASC) ,
   CONSTRAINT `fk_orders_users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `proyecto_integrador`.`users` (`id`)
+    REFERENCES `proyecto_integrador_db`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_cart`
     FOREIGN KEY (`cart_id`)
-    REFERENCES `proyecto_integrador`.`cart` (`id`)
+    REFERENCES `proyecto_integrador_db`.`cart` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_shipping`
     FOREIGN KEY (`shipping_id`)
-    REFERENCES `proyecto_integrador`.`shipping` (`id`)
+    REFERENCES `proyecto_integrador_db`.`shipping` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_payments`
     FOREIGN KEY (`payment_id`)
-    REFERENCES `proyecto_integrador`.`payments` (`id`)
+    REFERENCES `proyecto_integrador_db`.`payments` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_order_status`
     FOREIGN KEY (`order_status_id`)
-    REFERENCES `proyecto_integrador`.`order_status` (`id`)
+    REFERENCES `proyecto_integrador_db`.`order_status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
@@ -220,43 +220,43 @@ CREATE TABLE `proyecto_integrador`.`orders` (
 
 
 
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('1', 'Te Negro', '550.75', 'EarlGrey1.png');
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('2', 'Te Rojo', '2606.35', 'TeRojo1.jpg');
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('3', 'Te Verde', '1701.84', 'TeVerde3.png');
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('4', 'Te Oolong', '1621.56', 'oolong.jpg');
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('5', 'Te de Manzanilla', '2952.4', 'Manzanilla3.jpg');
-INSERT INTO `proyecto_integrador`.`products` (`id`, `name`, `price`, `image`) VALUES ('6', 'Te Blanco', ' 1777.16', 'TeBlanco1.jpg');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('1', 'Te Negro', '550.75', 'EarlGrey1.png');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('2', 'Te Rojo', '2606.35', 'TeRojo1.jpg');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('3', 'Te Verde', '1701.84', 'TeVerde3.png');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('4', 'Te Oolong', '1621.56', 'oolong.jpg');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('5', 'Te de Manzanilla', '2952.4', 'Manzanilla3.jpg');
+INSERT INTO `proyecto_integrador_db`.`products` (`id`, `name`, `price`, `image`) VALUES ('6', 'Te Blanco', ' 1777.16', 'TeBlanco1.jpg');
 
 
-ALTER TABLE `proyecto_integrador`.`users` 
+ALTER TABLE `proyecto_integrador_db`.`users` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`cart` 
+ALTER TABLE `proyecto_integrador_db`.`cart` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`extras` 
+ALTER TABLE `proyecto_integrador_db`.`extras` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`orders` 
+ALTER TABLE `proyecto_integrador_db`.`orders` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`payments` 
+ALTER TABLE 'proyecto_integrador_db`.`payments` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`products` 
+ALTER TABLE `proyecto_integrador_db`.`products` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`shipping` 
+ALTER TABLE `proyecto_integrador_db`.`shipping` 
 CHANGE COLUMN `creation` `created_at` TIMESTAMP NULL DEFAULT NULL ,
 CHANGE COLUMN `modification` `updated_at` TIMESTAMP NULL DEFAULT NULL ;
 
-ALTER TABLE `proyecto_integrador`.`orders` 
+ALTER TABLE `proyecto_integrador_db`.`orders` 
 DROP FOREIGN KEY `fk_orders_payments`,
 DROP FOREIGN KEY `fk_orders_shipping`;
 ALTER TABLE `proyecto_integrador`.`orders` 
@@ -275,22 +275,22 @@ ADD CONSTRAINT `fk_orders_shipping`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-DELETE FROM `proyecto_integrador`.`products` WHERE (`id` = '7');
-DELETE FROM `proyecto_integrador`.`products` WHERE (`id` = '8');
-DELETE FROM `proyecto_integrador`.`products` WHERE (`id` = '9');
-DELETE FROM `proyecto_integrador`.`products` WHERE (`id` = '13');
+DELETE FROM `proyecto_integrador_db`.`products` WHERE (`id` = '7');
+DELETE FROM `proyecto_integrador_db`.`products` WHERE (`id` = '8');
+DELETE FROM `proyecto_integrador_db`.`products` WHERE (`id` = '9');
+DELETE FROM `proyecto_integrador_db`.`products` WHERE (`id` = '13');
 
 /*DD deletd_at created_at and update_at in tables*/
-ALTER TABLE `proyecto_integrador`.`roles` ADD COLUMN `created_at` TIMESTAMP NULL DEFAULT NULL;
-ALTER TABLE `proyecto_integrador`.`roles` ADD COLUMN `updated_at` TIMESTAMP NULL DEFAULT NULL;
-ALTER TABLE `proyecto_integrador`.`roles` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`roles` ADD COLUMN `created_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`roles` ADD COLUMN `updated_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`roles` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 
 
-ALTER TABLE `proyecto_integrador`.`users` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`users` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 
-ALTER TABLE `proyecto_integrador`.`users_roles` ADD COLUMN `created_at` TIMESTAMP NULL DEFAULT NULL;
-ALTER TABLE `proyecto_integrador`.`users_roles` ADD COLUMN `updated_at` TIMESTAMP NULL DEFAULT NULL;
-ALTER TABLE `proyecto_integrador`.`users_roles` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`users_roles` ADD COLUMN `created_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`users_roles` ADD COLUMN `updated_at` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `proyecto_integrador_db`.`users_roles` ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 
 
 
@@ -378,3 +378,10 @@ ADD COLUMN `deleted_at` TIMESTAMP NULL DEFAULT NULL AFTER `password`;
 
 ALTER TABLE `proyecto_integrador_db`.`users_roles` 
 ADD COLUMN `delted_at` TIMESTAMP NULL DEFAULT NULL AFTER `role_id`;
+
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'El té blanco es antioxidante, mejora tu sistema inmunitario y frena el envejecimiento celular. En cuanto a su poder vitamínico, el té blanco tiene un gran aporte de vitamina C, que también refuerza el sistema inmunitario y te protege de resfriados y procesos víricos. Otro nutriente que forma parte de la composición del té blanco es la vitamina E, que está ligada a la mejora de los trastornos oculares, las funciones mentales y la salud cardiovascular.', `instructions` = 'Tiempo de infusión de 5 minutos, calentar el agua preferentemente a 75 grados. Ideal para la tarde.' WHERE (`id` = '6');
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'Una de las propiedades más destacadas del té negro es que contribuye a disminuir el nivel de colesterol en sangre. También, ayuda a controlar la presión sanguínea y es ideal para consumirse antes de las comidas, ya que prepara el aparato digestivo para trabajar correctamente.', `instructions` = 'Tiempo de infusión de 4 minutos. Calentar el agua preferentemente a 95 grados. Ideal para la mañana.' WHERE (`id` = '1');
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'El té rojo, también conocido como Pu Erh, ejerce una acción diurética que favorece la eliminación de toxinas y evita la retención de líquidos. Es perfecto para ayudarnos en el proceso de la digestión. Además, esta estimulación también acelera la metabolización de los alimentos. Además, era muy conocido antiguamente como remedio natural para reducir la grasa de la sangre.', `instructions` = 'Tiempo de infusión de 4 minutos. Calentar el agua preferentemente a 85 grados. Ideal para la tarde.' WHERE (`id` = '2');
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'El té verde posee un mayor poder antioxidantes, por eso ayuda a prevenir el envejecimiento. Es rico en polifenoles que tienen efectos como la reducción de la inflamación y protegen contra las enfermedades cardiovasculares y sus factores de riesgo, incluida la hipertensión arterial. Incrementa la formación de hueso. Además, con una alta concentración de vitamina C, el té verde fortalece al sistema inmunológico contra resfríos, gripes y otras infecciones.', `instructions` = 'Tiempo de infusión de 2 minutos, calentar el agua preferentemente a 85 grados. Ideal para el dia y la noche' WHERE (`id` = '3');
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'El té azul ayuda a fortalecer el sistema inmunológico. contribuye a reducir el colesterol, la presión arterial y el nivel de azúcar en la sangre. Por si fuera poco, se sabe que protege al hígado, mejora las defensas y contribuye a quemar grasas. También se lo utiliza para luchar contra el estrés, y su poder para fortalecer los huesos o limitar los efectos de la diabetes.', `instructions` = 'Tiempo de infusión de 7 minutos, calentar el agua preferentemente a 90 grados. Ideal para la mañana o la tarde' WHERE (`id` = '4');
+UPDATE `proyecto_integrador_db`.`products` SET `description` = 'Las principales propiedades de esta hierba son antiinflamatorias, antialérgicas, antibacterianas y sedantes. Además, posee buenas ventajas digestivas y es muy buena para tratar afecciones respiratorias como ser el asma, el resfrío y la fiebre. También, a aquellas personas que no pueden dormir se les aconseja que beban una infusión de manzanilla para conseguir sueños más profundos.', `instructions` = 'Tiempo de infusión de 2 minutos, calentar el agua preferentemente a 85 grados. Ideal para antes de irse a dormir' WHERE (`id` = '5');
